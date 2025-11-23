ee480fbd2aae321f761436976d2b6ea737526440
AnaCarolina79 <anacarolinamoreiraluna@gmail.com>
2025-11-23 19:26:43 -0300

    '.'

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import './style.scss'
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div >
+                <a href="">Inicio</a>
+                <a href="">Sobre</a>
+                <a href="">Projetos</a>
+                <a href="">Contato</a>
+            </div>
+        </nav>
+    );
+}
---
+++ app/components/presentation/index.tsx
@@ -0,0 +1,11 @@
+import './style.scss'
+
+export default function Presentation() {
+  return (
+    <div className='presentation'>
+    <p>Olá, eu sou a Ana Carolina Moreira</p>
+    <h1>DEV FULL-STACK</h1>
+        <h2><span>&</span> DESIGNER</h2>
+    </div>
+  );
+}
